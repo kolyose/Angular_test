ac062181594c9cf6356ec5e6848065faee6e8104
kols <kols@irace.com>
2017-01-21 14:00:06 +0200

    add/delete articles functionality

--- public/javascripts/articles.js
+++ public/javascripts/articles.js
@@ -1,8 +1,8 @@
-angular.module('articles', [])
+angular.module('articles', ['ui.bootstrap'])
 
 .service('articlesService', ArticlesService)
 
-.component('artciles', {
+.component('articles', {
     templateUrl: 'articles.html',
     $routeConfig: [
         {path: '/', name: 'ArticleList', component: 'articleList', useAsDefault: true},
@@ -12,13 +12,41 @@ angular.module('articles', [])
 
 .component('articleList', {
     templateUrl: "articleList.html",
-    controller(articlesService){    
+    controller(articlesService, $uibModal){    
         const $ctrl = this;
 
-        $ctrl.$routerOnActivate = (next) => {
+        this.$routerOnActivate = (next) => {
             return articlesService.getArticles().then(articles => {
                 $ctrl.articles = articles;
+
+                const deleteId = next.params.deleteId;
+                if (deleteId) this.deleteArticleById(deleteId);
             });
+        }
+
+        this.deleteArticleById = id => {
+            for (let i=0, length=$ctrl.articles.length; i<length; i++){
+                if ($ctrl.articles[i].id == id){
+                     $ctrl.articles.splice(i, 1);
+                    break;
+                }
+            }
+        }
+
+        this.addArticle = () => {
+            let modalInstance = $uibModal.open({
+                component: 'addArticle'
+            });
+
+            modalInstance.result.then(
+                (data) => {
+                    data.id = $ctrl.articles[$ctrl.articles.length-1].id + 1;
+                    $ctrl.articles.push(data);
+                }, 
+                (err) => {
+                    console.error(err);
+                }
+            );
         }
     }
 })
@@ -29,24 +57,26 @@ angular.module('articles', [])
         const $ctrl = this;
 
         this.$routerOnActivate = (next) => {
-            const id = next.params.id;
-            this.onDelete = next.params.onDelete;
+            this.id = next.params.id;
             
-            articlesService.getArticleById(id).then(article => {
+            articlesService.getArticleById(this.id).then(article => {                
                 $ctrl.article = article;
             });
         }   
 
         this.delete = () => {
-            this.onDelete(this.article.id);
+           this.$router.navigate(['ArticleList', {deleteId: this.id}]);
         } 
+    },
+    bindings: {
+        $router: '<' 
     }
 });
 
 function ArticlesService($q){
-    let articlesPromise = $.resolve([
-        {id:1, title: `Record!!`, description: "Record1 description"},
-        {id:2, title: `Hi, I'm a record 2!`, description: "I'm better than first one"}
+    let articlesPromise = $q.resolve([
+        {id:1, title: `Article #1`, description: "Hello, I'm an article number one! And I'm very interesting one."},
+        {id:2, title: `Article number two`, description: "Hi there! I'm much more intteresting article than the first one! Just try me ;)"}
     ]);
 
     this.getArticles = () => {
@@ -54,9 +84,9 @@ function ArticlesService($q){
     }
 
     this.getArticleById = (id) => {
-        return articlesPromise.then(articles => {
-            for (let article of articles){
-                if (article === id) return article;
+        return articlesPromise.then(articles => {           
+            for (let article of articles){             
+                if (article.id === id) return article;
             }
         });
     }
