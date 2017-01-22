9689e16d872d59e81daaef77655e7cb608037580
kolyose <kolyose@gmail.com>
2017-01-22 23:36:42 +0200

    passwords validation functionality added

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -1,12 +1,14 @@
 angular.module('auth', [])
 
 .service('authService', AuthService)
+.directive('isEqualTo', isEqualTo)
 
 .component('auth', {
     templateUrl: 'auth.html',
     controller(authService){
-        //authService.authorize();
-        this.user = {}
+        
+        this.login = {user:{}};
+        this.registration = {user:{}};
 
         this.login = () => {
             console.log("login")
@@ -32,5 +34,24 @@ function AuthService(){
 
     this.logout = () => {
         this.authorized = false;
+    }
+}
+
+function isEqualTo(){
+    return {
+        require: 'ngModel',
+        scope: {
+            isEqualTo: '='
+        },
+        link: function (scope, elem, attrs, ngModel){
+            ngModel.$validators.isEqualTo = function (modelValue, viewValue){
+                const result = (modelValue == scope.isEqualTo);
+                return result;
+            }
+
+            scope.$watch('isEqualTo', function(){
+                ngModel.$validate();
+            });
+        }
     }
 }
